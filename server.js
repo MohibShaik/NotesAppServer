f7363f9da1bdd91a4b641c05e6a254890fd42b1b
Mohib Shaik <mohib.shaik16@gmail.com>
2022-05-05 17:50:06 +0530

    changes in server file

--- server.js
+++ server.js
@@ -37,5 +37,6 @@ app.get('/', (req, res) => {
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
+  console.log('Hiii');
   console.log(`Server is running on port ${PORT}.`);
 });
